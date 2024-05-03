2e8829a1a85f63909ea9eeef3b016ae6a21ee3f4
kostiago <tiago_pb-@hotmail.com>
2024-05-03 14:30:36 -0300

    feat: add initial header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+import Logo from "../../public/logo (2).svg";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between px-5 pt-6">
+      <Image src={Logo} alt="food hub" height={300} width={100} />
+
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <HiOutlineMenuAlt3 height={50} width={50} />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
